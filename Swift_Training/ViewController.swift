20f6e87fbd5713ee1fcd8523fa3a5b0d8a6bd86e
funakoshi-dev <funakoshi.dev@gmail.com>
2021-10-17 00:03:02 +0900

    Add Cell to TableView. Fix label outlet.

--- Swift_Training/ViewController.swift
+++ Swift_Training/ViewController.swift
@@ -7,13 +7,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    @IBOutlet weak var testView: Test!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //作ったテーブルビューに作ったセルを紐づける
+        testView.testTableView.register(UINib(nibName: "testTableViewCell", bundle: nil), forCellReuseIdentifier: "testCell")
+        testView.testTableView.dataSource = self
+        testView.testTableView.delegate = self
     }
 
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "testCell", for: indexPath) as? testTableViewCell
+        else { return UITableViewCell() }
+        cell.labelL.text = "lllll"
+        cell.labelR.text = "rrrrr"
+        return cell
+    }
 }
 
